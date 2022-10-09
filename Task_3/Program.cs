61bba8bbc1faed410bef699636e46b049b40bc66
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-09 16:59:07 +0500

    Task 3 таблица кубов чисел от 1 до N

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void GetNumber (int number)
+{  
+    if (number >= 1)
+    {   
+        Console.Write($"\nТаблица кубов чисел от 1 до {number}:\n");
+        for (int i = 1; i <= number; i++)    
+        {
+            double square = Math.Pow(i,3);
+            Console.Write($" {square}");
+        }
+    }
+    else
+    Console.WriteLine("Введите валидное число, больше 1");
+    
+}
+Console.WriteLine("\nВведите число больше 1, чтобы получить таблицу кубов предыдущих чисел:\n");
+GetNumber(int.Parse(Console.ReadLine()??""));
